93362ea44c6461f10cb78d36f3a502c475981fa1
omertoso <lq1448705291@126.com>
2024-12-15 03:23:12 +0800

    yes

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://omertoso.github.io/",
-  base: "/" ,
+  site: "http://omertoso.top/",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(
